a9fd0a2944d748a1ea8968cdf1e14ef71750d944
Burhanuddin Salim <burhan.salman@gmail.com>
2025-12-24 15:48:12 +0400

    feat: add prisma client

--- server/router.ts
+++ server/router.ts
@@ -4,9 +4,4 @@ const router = express.Router();
 
 router.get("/health", (_, res) => res.send("OK"));
 
-router.get("/trackers");
-router.get("/trackers/:id");
-router.post("/trackers");
-router.get("/trackers/:id/logs");
-
 export default router;
